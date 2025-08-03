7593f810b05497260d98961dd8ee709c19f8bc72
Anji Kumar Sharma <anjit.krs@gmail.com>
2025-08-03 09:31:21 +0530

    complete task

---
+++ app/utils/chuck_file.py
@@ -0,0 +1,17 @@
+from app.configuration.config import settings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from typing import List
+
+class ChunkFile:
+    def __init__(self):
+        self.chunk_size=settings.chunk_size
+        self.chunk_overlap=settings.chunk_overlap
+
+    def recursive_chunking(self, docs)-> List[str]:
+        splitter = RecursiveCharacterTextSplitter(
+            chunk_size=self.chunk_size,
+            chunk_overlap=self.chunk_overlap
+        )
+        print(f"Chunking documents into size: {self.chunk_size} with overlap: {self.chunk_overlap}")
+        return splitter.split_documents(docs)
+
